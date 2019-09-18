9c2c82a81de1d20a0ae809e127f15add7383a715
Kevin <tkevinkiragu@gmail.com>
2019-09-19 00:45:54 +0300

    Random improvement

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Get year
 const year = new Date().getFullYear();
 document.querySelector(".year").textContent = year;
+
+// Map
+var map;
+function initMap() {
+	map = new google.maps.Map(document.querySelector(".map"), {
+		center: { lat: -1.2839974, lng: 36.8044067 },
+		zoom: 8
+	});
+}
